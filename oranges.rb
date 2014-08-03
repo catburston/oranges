c89a72e6c271fe0b7e32c0191a3f2f2aa6c9a5fa
catburston <cat1788@gmail.com>
2014-08-03 21:16:24 +0200

    Tree class, fruit trees inherit from Tree

--- oranges.rb
+++ oranges.rb
@@ -1,30 +1,53 @@
-class OrangeTree
-
-	attr_reader :height, :age, :oranges
-		#Only for variables that will be accessed outside the class
-
+class Tree
+	attr_reader  :height, :age
 	def initialize
-		@height = 0		#put @ if diff methods will use
+		@height = 0
 		@age = 0
-		@oranges = []
+		@max_growable_age = 0
+		@death_age = 0
+		@annual_growth = 0
 	end
 
-	def age! 					#Increase age only
+	def age!
 		@age += 1
 		growable
-		add_orange if can_bear_fruit?
+		# add_orange if can_bear_fruit?
 	end
 
-	def height! 			#Increase height only
-		@height += 10
-	end
-
-	def growable 			#Tells you if can or cannot grow
-		height! if @age <= 15
+	def growable
+		height! if @age <= @max_growable_age
 	end
 
 	def dead?
-		@age >= 20
+		@age >= @death_age
+	end
+
+	def height!
+		@height += @annual_growth
+	end
+end
+
+
+
+class AppleTree < Tree
+	attr_reader :apples
+	def initialize
+		super
+		@max_growable_age = 25
+		@death_age = 30
+		@annual_growth = 3
+		@apples = []
+	end
+end
+
+class OrangeTree < Tree
+	attr_reader :oranges
+	def initialize
+		super
+		@max_growable_age = 15
+		@death_age = 20
+		@annual_growth = 10
+		@oranges = []
 	end
 
 	def can_bear_fruit?
@@ -48,17 +71,21 @@ class OrangeTree
 
 end
 
-class Orange
-
+class Fruit
 	attr_reader :diameter
-
 	def initialize
 		@diameter = 1
 	end
+end
 
+class Orange < Fruit
+end
+
+class Apple < Fruit
 end
 
 ##TEST FOR ORANGE TREE
+puts "==============================ORANGE"
 tree = OrangeTree.new
 puts tree.height == 0
 puts tree.age == 0
@@ -95,6 +122,46 @@ puts tree.can_bear_fruit? == false
 
 orange = Orange.new
 puts orange.diameter == 1
+
+##TEST FOR APPLE TREE
+puts "==============================APPLE"
+tree2 = AppleTree.new
+# puts tree.height
+puts tree2.height == 0
+puts tree2.age == 0
+
+tree2.age!
+puts tree2.age == 1
+puts tree2.height == 3
+
+# puts tree2.pick_apple!
+# puts tree2.can_bear_fruit? == false
+# puts tree2.any_apple? == false
+
+# 16.times do
+# 	tree2.age!
+# end
+
+# puts tree2.can_bear_fruit? == true
+
+# puts tree2.age
+# puts tree2.apples.length
+
+# tree2.pick_apple!
+# puts tree2.apples.length
+
+# puts tree2.age == 17
+# puts tree2.height == 150
+
+# 5.times do
+# 	tree2.age!
+# end
+
+# puts tree2.dead? == true
+# puts tree2.can_bear_fruit? == false
+
+# apple = Apple.new
+# puts apple.diameter == 1
 
 
 # tree.age! until tree.any_oranges?
